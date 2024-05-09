962e9c9af7792c1f0f90cbc58edb4dbdd7556808
sharpsailor <sharpsailor13@gmail.com>
2024-05-09 20:48:26 +0530

    Adjusted the Div CSS

--- src/components/TodoGroup.vue
+++ src/components/TodoGroup.vue
@@ -1,87 +1,91 @@
 <template>
-  <div class="group-wrapper">
-    <h3>{{ groupLabel[props.status]}} : {{ getCountForStatus(props.status)}}</h3>
-
-
-    <Draggable
-      class="draggable"
-      :list="todoList"
-      group="todos"
-      itemKey="id"
-      @change="onDraggableChange"
-    >
-      <template #item="{ element: todo }">
-        <li>
-          {{ todo.title }}
-          <span class="delete-icon" @click="deleteTodo(todo)">x</span>
-          <div>
-            <span class="todo-description">{{ todo.description }}</span>
-          </div>
-        </li>
-      </template>
-    </Draggable>
-
+  <div class="main-group-wrapper">
+    <h3>{{ groupLabel[props.status] }} : {{ getCountForStatus(props.status) }}</h3>
+    <div class="group-wrapper scrollable">
+      <Draggable
+        class="draggable"
+        :list="todoList"
+        group="todos"
+        itemKey="id"
+        @change="onDraggableChange"
+      >
+        <template #item="{ element: todo }">
+          <li>
+            {{ todo.title }}
+            <span class="delete-icon" @click="deleteTodo(todo)">x</span>
+            <div>
+              <span class="todo-description">{{ todo.description }}</span>
+            </div>
+          </li>
+        </template>
+      </Draggable>
+    </div>
     <CreateTodo :status="props.status" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { TodoStatus } from "@/types";
-import { ref, defineProps, computed } from "vue";
-import Draggable from "vuedraggable";
-import useTodos from "@/store/useTodos";
-import CreateTodo from "./CreateTodo.vue";
+import { TodoStatus } from '@/types'
+import { ref, defineProps, computed } from 'vue'
+import Draggable from 'vuedraggable'
+import useTodos from '@/store/useTodos'
+import CreateTodo from './CreateTodo.vue'
 
 interface Props {
-  status: TodoStatus;
+  status: TodoStatus
 }
 
-const props = defineProps<Props>();
+const props = defineProps<Props>()
 
-const { getTodosByStatus, deleteTodo, updateTodo } = useTodos();
-const todoList = getTodosByStatus(props.status);
+const { getTodosByStatus, deleteTodo, updateTodo } = useTodos()
+const todoList = getTodosByStatus(props.status)
 
 const groupLabel = {
-  [TodoStatus.Pending]: "Pending",
-  [TodoStatus.InProgress]: "In Progress",
-  [TodoStatus.Completed]: "Completed",
-};
+  [TodoStatus.Pending]: 'Pending',
+  [TodoStatus.InProgress]: 'In Progress',
+  [TodoStatus.Completed]: 'Completed'
+}
 
 const onDraggableChange = (payload: any) => {
   if (payload?.added?.element?.status) {
-    updateTodo(payload?.added?.element, props.status);
+    updateTodo(payload?.added?.element, props.status)
   }
-};
+}
 
 // Compute count of tasks for each status
 const pendingCount = computed(() => {
-  return todoList.value.filter(todo => todo.status === TodoStatus.Pending).length;
-});
+  return todoList.value.filter((todo) => todo.status === TodoStatus.Pending).length
+})
 
 const inProgressCount = computed(() => {
-  return todoList.value.filter(todo => todo.status === TodoStatus.InProgress).length;
-});
+  return todoList.value.filter((todo) => todo.status === TodoStatus.InProgress).length
+})
 
 const completedCount = computed(() => {
-  return todoList.value.filter(todo => todo.status === TodoStatus.Completed).length;
-});
+  return todoList.value.filter((todo) => todo.status === TodoStatus.Completed).length
+})
 
 // Method to get count for a specific status
 const getCountForStatus = (status: TodoStatus) => {
   switch (status) {
     case TodoStatus.Pending:
-      return pendingCount.value;
+      return pendingCount.value
     case TodoStatus.InProgress:
-      return inProgressCount.value;
+      return inProgressCount.value
     case TodoStatus.Completed:
-      return completedCount.value;
+      return completedCount.value
     default:
-      return 0;
+      return 0
   }
-};
+}
 </script>
 
 <style scoped>
+.main-group-wrapper {
+  background-color: black;
+  border-radius: 10px;
+  padding: 20px;
+}
 .group-wrapper {
   flex: 1;
   padding: 20px;
@@ -99,7 +103,10 @@ const getCountForStatus = (status: TodoStatus) => {
   margin-bottom: 10px;
   border-radius: 8px;
 }
-
+.scrollable {
+  height: 300px;
+  overflow-y: auto;
+}
 .draggable {
   min-height: 200px;
 }
